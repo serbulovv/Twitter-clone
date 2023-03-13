a08d44efe1712ec1ec964b7b199f42514d3cca8a
serulovv <mellnote71@gmail.com>
2023-03-13 20:49:22 +0200

    Added styles to dashboard objects

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,12 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  def time_ago_in_words
+    time_diff_in_seconds = (Time.zone.parse(DateTime.now.to_s) - created_at)
+    hours = (time_diff_in_seconds / 1.hour).round
+    return "#{hours}h" if hours.positive?
+
+    minutes = (time_diff_in_seconds / 1.minute).round
+    "#{minutes}min"
+  end
 end
